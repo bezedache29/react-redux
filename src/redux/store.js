2a022207e6895683801e53f10423c7be6a526bad
Christophe Salou <bezedache29@gmail.com>
2021-09-28 11:53:45 +0200

    Redux TV

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,13 @@
-import { createStore } from "redux"
+import { createStore, combineReducers } from "redux"
 import phoneReducer from "./phone/reducerPhone"
+import tvReducer from "./tv/reducerTv"
 
-const store = createStore(phoneReducer)
+// On merge les 2 reducers pour les passer a la création de store
+const routeReducer = combineReducers({
+  phones: phoneReducer,
+  tv: tvReducer
+})
+
+const store = createStore(routeReducer)
 
 export default store
---
+++ src/redux/tv/reducerTv.js
@@ -0,0 +1,24 @@
+import { BUY_TV } from './type'
+
+const initialStateTv = {
+  tv: 20
+}
+
+const tvReducer = (state = initialStateTv, action) => {
+
+  switch(action.type) {
+
+    case BUY_TV:
+      return {
+        ...state,
+        tv: state.tv - 1
+      }
+
+    default :
+      return state
+
+  }
+
+}
+
+export default tvReducer
